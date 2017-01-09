4f87f17ed629887e8caab77e42297aaecf3fb930
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-01-09 09:30:07 +0100

    Karma fixed paths

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -1,30 +1,30 @@
-// Run this file standalone with: node_modules\.bin\karma start karma.conf.js
+// Run this file standalone with: node_modules\.bin\karma start karma.conf.js. This requires the basePath config to be set.
 module.exports = function(config) {
     config.set({
-        basePath: '',
+        //basePath: '',
         frameworks: ['jasmine'],
         files: [
             /* vendor */
-            //'_test/vendor/jquery.min.js',
-            //'_test/vendor/knockout-3.2.0.js',
+            //'test/vendor/jquery.min.js',
+            //'test/vendor/knockout-3.2.0.js',
             /* helpers */
-            //'_test/helpers/*.js',
+            //'test/helpers/*.js',
             /* sources */
-            //'_js/util/*.js',
-            //'_js/viewmodels/*.js',
-            '_js/app.js',
+            //'js/util/*.js',
+            //'js/viewmodels/*.js',
+            'js/app.js',
             /* spec */
-            //'_test/util/*Spec.js',
-            //'_test/viewmodels/*Spec.js',
-            '_test/appSpec.js'
+            //'test/util/*Spec.js',
+            //'test/viewmodels/*Spec.js',
+            'test/appSpec.js'
         ],
         browsers: ['PhantomJS'],//['PhantomJS', 'Chrome']
         singleRun: true,
         reporters: ['progress', 'coverage'],
-        preprocessors: { '_js/**/*.js': ['coverage'] },
+        preprocessors: { 'js/**/*.js': ['coverage'] },
         coverageReporter: {
             reporters: [
-                { type: 'html', dir: '_analysis/coverage/' },
+                { type: 'html', dir: '../../../../../../analysis/coverage/' },
                 { type: 'text-summary' }
             ]
         }
