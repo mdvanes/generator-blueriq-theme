d282cb8d2773f60e5e12815b078e1410bd55d58d
M.D. van Es <es@mdworld.nl>
2017-01-09 10:24:03 +0100

    Upgrade yeoman to make yarn installation possible, see #10

--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
-/**
- * Created by m.van.es on 12-10-2015.
- */
+#!/usr/bin/env node
 
 /*
     Sets up the basic toolchain.
@@ -13,40 +11,36 @@
         * in any dir run ```yo blueriq-theme```
  */
 
-var generators = require('yeoman-generator'),
+var Generator = require('yeoman-generator'),
     yosay = require('yosay'),
     path = require('path'),
     chalk = require('chalk'),
     pkg = require('../package.json');
 
-// an example: https://github.com/yeoman/generator-gruntplugin/blob/master/app/index.js
+// TODO test adding a subgenerator
 
-module.exports = generators.Base.extend({
-    intro: function() {
+module.exports = class extends Generator {
+    intro() {
         console.log(yosay('Welcome to the Blueriq Theme generator v' + pkg.version + '!'));
-    },
-    askFor: function() {
-        var cb = this.async();
-        var prompts = [
-            {
-                name: 'name',
-                message: 'project name'
-            }
-        ];
+    }
 
-        var self = this;
-
-        this.prompt(prompts, function(props) {
-            self.props = props;
-            cb();
+    askFor() {
+        return this.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'project name',
+            default : this.appname // Default to current folder name
+        }]).then(props => {
+            this.props = props;
         });
-    },
-    writePackageJson: function() {
-        var userName = process.env['USERPROFILE'].split(path.sep).pop();
+    }
+
+    writePackageJson() {
+        let userName = process.env['USERPROFILE'].split(path.sep).pop();
         if(!userName || userName.length === '0' || userName === 'm.van.es') {
             userName = 'M.D. van Es';
         }
-        var pkgFile = {
+        const pkgFile = {
             name: this.props.name,
             version: '0.0.0',
             author: userName,
@@ -58,11 +52,11 @@ module.exports = generators.Base.extend({
             private: true,
             license: 'closed'
         };
-        this.write('package.json', JSON.stringify(pkgFile, null, 4));
-    },
-    installDevDependencies: function() {
-        var self = this;
-        this.npmInstall([
+        this.fs.writeJSON('package.json', pkgFile);
+    }
+
+    installDevDependencies() {
+        const dependencies = [
             'grunt',
             'grunt-browser-sync',
             'grunt-contrib-imagemin',
@@ -74,15 +68,17 @@ module.exports = generators.Base.extend({
             'grunt-sass', 'grunt-stylelint',
             'jasmine-core', 'karma', 'karma-coverage', 'karma-jasmine', 'karma-phantomjs-launcher', 'grunt-karma',
             'load-grunt-tasks',
-            'time-grunt'],
+            'time-grunt'];
+        this.npmInstall(dependencies,
             { 'saveDev': true },
-            function() {
+            () => {
                 console.log('Dev dependency installation completed.');
-                self.outtro(); // Show the outtro a second time, after installing the packages.
+                this.outtro(); // Show the outtro a second time, after installing the packages.
             }
         );
-    },
-    copyTemplates: function() {
+    }
+
+    copyTemplates() {
         // UI
         this.fs.copyTpl(
             this.templatePath('stg/template.stg'),
@@ -119,8 +115,9 @@ module.exports = generators.Base.extend({
             this.destinationPath('setupSymLinks.bat'),
             { projectName: this.props.name }
         );
-    },
-    outtro: function() {
+    }
+
+    outtro() {
         console.log(chalk.bold.green('Run `grunt` and visit the server at http://localhost:8282/webresources/mvc/v2/themes/' + this.props.name + '/src/stubs/'));
     }
-});
+};
